d914108945f7e5a06f0239146776ea0ce331cce0
Srutheka <srutheka.polasani@gmail.com>
2020-04-06 12:55:07 +0530

    First commit

---
+++ coursemanagement/src/main/java/com/elearning/coursemanagement/controllers/AdminController.java
@@ -0,0 +1,121 @@
+package com.elearning.coursemanagement.controllers;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.elearning.coursemanagement.model.Admin;
+import com.elearning.coursemanagement.model.Course;
+import com.elearning.coursemanagement.model.Student;
+import com.elearning.coursemanagement.services.AdminService;
+
+@Controller
+@RequestMapping(value = "/admin")
+public class AdminController {
+	
+	@Autowired
+	private AdminService adminServices;
+	
+	@GetMapping("/")
+
+	public String adminLogin(Model model)
+	{
+	  Admin admin = new Admin();
+	  model.addAttribute("admin", admin);
+	  return "admin";
+	}
+
+	
+	@PostMapping("/adminlogin")
+	public String VerifyAdminLogin(@Valid @ModelAttribute("admin") Admin admin, BindingResult result, Model model, HttpSession session)
+	{
+		
+		if(result.hasErrors())
+		{
+			return "admin";
+		}
+		else {
+		
+		if(adminServices.login(admin))
+		{
+			session.setAttribute("adminusername", admin.getUsername());
+			session.setMaxInactiveInterval(120);
+			//model.addAttribute("adminusername", admin.getUsername());
+			return "adminhome";
+		}
+		model.addAttribute("message", "Invalid UserName or Password");
+		//System.out.println(admin);
+		return "failure";
+		}
+	}
+	
+	@GetMapping("/addCourse")
+	public String addCourse(Model model,HttpSession session)
+	{
+		String username=(String)session.getAttribute("adminusername");
+		if(username==null)
+		{
+			model.addAttribute("course", new Course());
+			model.addAttribute("addmessage", "Pleas Login First <a href='/admin/'>");
+			return "adminhome";
+		}
+		model.addAttribute("course", new Course());
+		return "addcourse";
+	}
+	 @PostMapping("/addcourse1")
+	    public String createCourse(@ModelAttribute("course") Course course,Model model)
+	    {
+	        // calculated the duration between dates	        
+	        long diff = course.getEndDate().getTime()-course.getStartDate().getTime();
+	        int duration = (int)(diff/(1000*60*60*24));
+	    course.setDuration(duration);
+	        
+	        boolean status = adminServices.addCourse(course);
+	        
+	        if(status)
+	        {
+	            model.addAttribute("coursemessage", "Course Added");
+	            return "adminhome";
+	        }
+	        else
+	        {
+	            model.addAttribute("coursemessage", "Course Not  Added");
+	            return "adminhome";
+	        }
+	    }
+	 @GetMapping("/students")
+	 public String getStudents(Model model,HttpSession session)
+	 {
+		 List<Student> stdList =adminServices.studentList();
+		 
+		 model.addAttribute("studentsList",stdList);
+		 return "adminhome";
+	 }
+      
+	 @GetMapping("/courses")
+	 public String getCourses(Model model,HttpSession session)
+	 {
+		 List<Course> cList =adminServices.courseList();
+		 
+		 model.addAttribute("coursesList",cList);
+		 return "adminhome";
+	 }
+
+	@GetMapping("/adminLogout")
+	public String adminLogOut(HttpSession session)
+	{
+		session.invalidate();
+		
+		return "redirect:/";
+	}
+	
+}
